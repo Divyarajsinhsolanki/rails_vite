3c6366c8fac8e4e488ffad3e38775dfcddd26d45
Divyarajsinhsolanki <solanki.divyarajsinhp@gmail.com>
2025-07-15 20:10:25 +0530

    Code clean

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :profile_picture
-  has_many :posts
+  has_many :posts, dependent: :destroy
   has_many :tasks, foreign_key: :assigned_to_user
   has_many :items
 
