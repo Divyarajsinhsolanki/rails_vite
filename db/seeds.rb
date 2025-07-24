75267ba38d0d61cab074cff59a07fbf412673c28
Divyarajsinhsolanki <90916607+Divyarajsinhsolanki@users.noreply.github.com>
2025-07-24 02:51:49 -0700

    Add default records to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,24 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Default Roles
+%w[owner admin member].each do |name|
+  Role.find_or_create_by!(name: name)
+end
+
+# Default Teams
+%w[ROR React Python QA DevOps Design].each do |name|
+  Team.find_or_create_by!(name: name)
+end
+
+# Default Projects
+[
+  "MyForms",
+  "AI",
+  "AC Space",
+  "Portal",
+  "DataHub"
+].each do |name|
+  Project.find_or_create_by!(name: name)
+end
