11e708d5c7da31553645643961cad4a1629c16f1
Divyarajsinhsolanki <90916607+Divyarajsinhsolanki@users.noreply.github.com>
2025-07-23 23:02:23 -0700

    Add email confirmation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,6 +94,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  Rails.application.routes.default_url_options[:host] = ENV.fetch("APP_HOST", "example.com")
+
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
