e37b4ceb82d8f5bda828f8e53f739fa091fde5ed
Divyarajsinhsolanki <solanki.divyarajsinhp@gmail.com>
2025-08-07 14:52:18 +0530

    Code clean

--- app/models/work_log.rb
+++ app/models/work_log.rb
@@ -9,4 +9,15 @@ class WorkLog < ApplicationRecord
   has_many :tags, through: :work_log_tags, source: :work_tag, class_name: 'WorkTag'
 
   validates :title, :log_date, :start_time, :end_time, presence: true
+  validate :end_time_after_start_time
+
+  private
+
+  def end_time_after_start_time
+    return if end_time.blank? || start_time.blank?
+
+    if end_time <= start_time
+      errors.add(:end_time, "must be after start time")
+    end
+  end
 end
