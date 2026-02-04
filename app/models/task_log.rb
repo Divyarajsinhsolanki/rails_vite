cb9396208c9a4b168665b8792eb75a561af725d9
Divyarajsinhsolanki <90916607+Divyarajsinhsolanki@users.noreply.github.com>
2026-02-04 17:30:00 +0530

    Sync task dates from code logs

--- app/models/task_log.rb
+++ app/models/task_log.rb
@@ -9,17 +9,46 @@ class TaskLog < ApplicationRecord
   validates :task, :developer, :log_date, :hours_logged, presence: true
   validates :hours_logged, numericality: true
 
-  after_save :sync_task_dates, if: -> { type == 'Code' }
+  before_destroy :store_task_date_sync_context
+  after_commit :sync_task_dates, on: [:create, :update, :destroy]
 
   private
 
   def sync_task_dates
-    return unless task
+    task_ids = []
+    if destroyed?
+      return unless @destroyed_type == 'Code'
 
-    code_logs = task.task_logs.where(type: 'Code')
-    task.update(
-      start_date: code_logs.minimum(:log_date),
-      end_date: code_logs.maximum(:log_date)
-    )
+      task_ids << @destroyed_task_id
+    else
+      return unless code_log_change?
+
+      task_ids << task_id
+      if (previous_task_id = previous_changes.dig('task_id', 0))
+        task_ids << previous_task_id
+      end
+    end
+
+    task_ids.compact.uniq.each do |task_id|
+      task = Task.find_by(id: task_id)
+      next unless task
+
+      code_logs = task.task_logs.where(type: 'Code')
+      task.update(
+        start_date: code_logs.minimum(:log_date),
+        end_date: code_logs.maximum(:log_date)
+      )
+    end
+  end
+
+  def code_log_change?
+    return true if type == 'Code'
+
+    previous_changes['type']&.include?('Code')
+  end
+
+  def store_task_date_sync_context
+    @destroyed_task_id = task_id
+    @destroyed_type = type
   end
 end
